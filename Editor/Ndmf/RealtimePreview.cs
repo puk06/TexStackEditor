80a2b124591b57175a1c5d1317637cf540c12cdf
Puk06 <86549420+puk06@users.noreply.github.com>
2026-05-27 17:56:14 +0900

    fix: correct logical operator for WhatChanged property in TextureReplacerNode

--- Editor/Ndmf/RealtimePreview.cs
+++ Editor/Ndmf/RealtimePreview.cs
@@ -132,7 +132,7 @@ namespace net.puk06.TexStackEditor.Editor.Ndmf
             private IEnumerable<ExtendedRenderTexture>? _processedTextures;
             private Dictionary<Renderer, Material?[]>? _processedMaterialDictionary;
 
-            public RenderAspects WhatChanged { get; private set; } = RenderAspects.Texture & RenderAspects.Material;
+            public RenderAspects WhatChanged { get; private set; } = RenderAspects.Texture | RenderAspects.Material;
 
             public TextureReplacerNode(Dictionary<Renderer, Material?[]>? processedMaterialDictionary, IEnumerable<ExtendedRenderTexture>? processedTexturesDictionary)
             {
